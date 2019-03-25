912f5924025ef796f855c33c2b9267329c48b8b9
mhsJBerg <48916553+mhsJBerg@users.noreply.github.com>
2019-03-25 09:43:05 -0400

    Add files via upload

---
+++ Snowman/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  // put setup code here
+  createCanvas(800,600);
+}
+
+function draw() {
+  // put drawing code here
+  fill(0,0,0);
+  rect(0,370,398,28);
+
+// snowman body
+  fill(255,0,0)
+ellipse(200, 300, 150, 150);
+fill(0,0,255);
+ellipse(200, 200, 100, 100);
+fill(0,255,0);
+ellipse(200, 120, 75, 75);
+line(150,200,50,100);
+line(250,200,350,100);
+//right arm
+//ground
+}
