b0837245343caea2a8137de9cbaa26cdb6669afa
mhsJBerg <48916553+mhsJBerg@users.noreply.github.com>
2019-04-23 09:57:17 -0400

    Add files via upload

---
+++ MOUSE SUFF/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  // put setup code here
+  createCanvas(800,600);
+  background(49,209,124);
+}
+
+function draw() {
+  // put drawing code here
+
+stroke(49,209,124);
+
+fill(mouseX,0,0);
+ellipse(mouseX,mouseY,100,100);
+
+if(mouseIsPressed){
+  if(mouseButton===LEFT){
+  background(random)}
+
+}
+}
