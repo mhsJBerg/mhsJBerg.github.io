f8865362fc673aaacdafccfbfdbfa34b0344568b
mhsJBerg <48916553+mhsJBerg@users.noreply.github.com>
2019-04-25 09:44:34 -0400

    Add files via upload

---
+++ Sound and Image/sketch.js
@@ -0,0 +1,16 @@
+function preload(){
+
+  img = loadImage('1345026_thumb.png');
+}
+
+
+function setup() {
+  // put setup code here
+  createCanvas(800,600);
+  background(0);
+}
+
+
+function draw() {
+  // put drawing code here
+}
